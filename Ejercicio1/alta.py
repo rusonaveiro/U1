2032a51160e6d2871113316931ca9bdd75e4c4ce
Diego Naveiro <66359329+rusonaveiro@users.noreply.github.com>
2020-07-27 15:11:38 +0200

    lunes

--- Ejercicio1/alta.py
+++ Ejercicio1/alta.py
@@ -57,6 +57,7 @@ def borrar_reg(tree):
     a = tree.item(id)["text"]   # Le asigno ese registro completo a la variable a
     tree.delete(id)  # borro el registro del tree pero no de la db
     print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
     base = sqlite3.connect('baseprueba3.db')
     # base = mysql.connector.connect(host="localhost", user="root", passwd="", baseprueba3)
     cursor = base.cursor()
@@ -67,23 +68,32 @@ def borrar_reg(tree):
 
 
 
-""" NO FUNCIONA
+""" NO FUNCIONA 
 
 def editar_reg(tree):
-    global id
     id = tree.focus()
     x = tree.get_children()
     for element in x:  # Cambiando los children del root item
         tree.delete(element)
-        tree.insert("", 0, text=str(id[0]), values=(id[1], id[2]))
 
     base = sqlite3.connect('baseprueba3.db')
     # base = mysql.connector.connect(host="localhost", user="root", passwd="", baseprueba3)
     cursor = base.cursor()
-    sql = 'UPDATE producto SET producto = ? where id = ?'
-    datos = (id[1])
+    # sql = 'UPDATE producto SET titulo=?, descripcion=? WHERE id = ' + id
+    datos = (tit.get(), des.get())
     cursor.execute(sql, datos)
     base.commit()
-    base.close()
+
+    sql_id = "SELECT * FROM producto"
+    cursor.execute(sql_id)
+    ingresos = cursor.fetchall()
+    global id
+        for i in ingresos:
+            print(i)
+            id = i
+            tree.insert("", "end", text=str(id[0]), values=(id[1], id[2]))
+
     print(cursor.rowcount, "Cantidad de registros afectados")
+    Ent1.delete(0, "end")
+    Ent2.delete(0, "end")
 """
